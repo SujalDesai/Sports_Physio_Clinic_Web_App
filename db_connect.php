56cf2b3b4e1463da54e885ad32d2c560fb7040ce
SujalDesai <158669886+SujalDesai@users.noreply.github.com>
2025-12-28 18:09:07 +0530

    minor update

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db   = "phydb";  // NEW database name
+$db   = "phydb"; 
 
 $conn = new mysqli($host, $user, $pass, $db);
 
@@ -10,3 +10,4 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
