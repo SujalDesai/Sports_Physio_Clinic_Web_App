6bd19c62e1b22dc221b340555c9d3ddda1df88a6
SujalDesai <158669886+SujalDesai@users.noreply.github.com>
2025-12-28 18:02:12 +0530

    Update
    Since multiple visualizations were generated, I added minimal labels to clearly differentiate each output.

--- clinic_eda.py
+++ clinic_eda.py
@@ -3,37 +3,36 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import mysql.connector
 
-# 1. Connect to MySQL with your credentials
+
+
 try:
     conn = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="",        # empty if root has no password in XAMPP
+    password="",       
     database="phydb"
 )
-
     print("✅ Connection successful!")
+    
 except mysql.connector.Error as err:
     print(f"❌ Error: {err}")
     exit()
 
-# 2. Load data into Pandas
 query = "SELECT * FROM appointments"
 df = pd.read_sql(query, conn)
 
-# 3. Service Popularity
+# Service Popularity
 plt.figure(figsize=(6,4))
 sns.countplot(data=df, x="service")
 plt.title("Service Popularity")
-plt.xlabel("Service Type")  # Optional: cleaner label
+plt.xlabel("Service Type")  
 plt.ylabel("Count")
-plt.xticks(rotation=0)      # 🔧 Set rotation to 0 for horizontal labels
+plt.xticks(rotation=0)     
 plt.tight_layout()
 plt.savefig("service_popularity.png")
 
 
-# 4. Peak Hours
-# Convert "0 days 18:45:00" → "18:45:00" → hour = 18
+# Peak Hours
 df['hour'] = df['appointment_time'].astype(str).str.extract(r'(\d{2}):')[0].astype(int)
 
 plt.figure(figsize=(6,4))
@@ -45,7 +44,7 @@ plt.savefig("peak_hours.png")
 
 
 
-# 5. Monthly Trends
+# Monthly Trends
 df['month'] = pd.to_datetime(df['appointment_date']).dt.month
 plt.figure(figsize=(6,4))
 sns.countplot(data=df, x="month", color="green" , width=0.35)
@@ -54,7 +53,7 @@ plt.xlabel("Month")
 plt.tight_layout()
 plt.savefig("monthly_trends.png")
 
-# 6. Cancellation Trends
+# Cancellation Trends
 plt.figure(figsize=(6,4))
 sns.countplot(data=df, x="status", palette="Set2")
 plt.title("Cancellation vs Confirmation")
@@ -62,3 +61,4 @@ plt.tight_layout()
 plt.savefig("cancellation_trends.png")
 
 print("📊 EDA charts generated successfully!")
+
